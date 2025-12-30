efc46b3768b0ee6f4e0044251b8378f07b94ae21
jlbuild <juliabuildbot@gmail.com>
2025-12-30 16:21:50 +0000

    Deno_jll build 2.6.3+0

--- src/Deno_jll.jl
+++ src/Deno_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Deno")
-JLLWrappers.@generate_main_file("Deno", UUID("04572ae6-984a-583e-9378-9577a1c2574d"))
+JLLWrappers.@generate_main_file("Deno", Base.UUID("04572ae6-984a-583e-9378-9577a1c2574d"))
 end  # module Deno_jll
